99bca49ad593d91c46376db68b581e8fbc4fc51c
Dutkercz <dutkercz@gmail.com>
2025-02-14 01:21:09 -0300

    Project Start

---
+++ src/main/java/Dev2025/PlayMuzics/PlayMuzicsApplication.java
@@ -0,0 +1,13 @@
+package Dev2025.PlayMuzics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayMuzicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayMuzicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Dev2025/PlayMuzics/PlayMuzicsApplicationTests.java
@@ -0,0 +1,13 @@
+package Dev2025.PlayMuzics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayMuzicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
