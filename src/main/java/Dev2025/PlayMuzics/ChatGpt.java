9ebfaf566c56fc28e3e3afc18e6f7c533425753c
Dutkercz <dutkercz@gmail.com>
2025-02-15 02:56:27 -0300

    Chat gpt works

---
+++ src/main/java/Dev2025/PlayMuzics/ChatGpt.java
@@ -0,0 +1,21 @@
+package Dev2025.PlayMuzics;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ChatGpt {
+    private final ChatClient chatClient;
+
+    public ChatGpt(ChatClient.Builder builder){
+        this.chatClient = builder.build();
+    }
+
+    public ChatClient getChatClient() {
+        return chatClient;
+    }
+
+    public String askChat (String aks){
+        return chatClient.prompt().user(aks).call().content();
+    }
+}
--- src/main/java/Dev2025/PlayMuzics/PlayMuzicsApplication.java
+++ src/main/java/Dev2025/PlayMuzics/PlayMuzicsApplication.java
@@ -1,5 +1,6 @@
 package Dev2025.PlayMuzics;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,16 +8,54 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Scanner;
 
 @SpringBootApplication
-public class PlayMuzicsApplication implements CommandLineRunner {
+public class PlayMuzicsApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlayMuzicsApplication.class, args);
 	}
+	@Autowired
+	private ChatGpt gpt;
+	private final Scanner scanner = new Scanner(System.in);
 
 	@Override
 	public void run(String... args) throws Exception {
-		Scanner scanner = new Scanner(System.in);
-		
-		String response = scanner.nextLine();
+		while (true){
+			System.out.print("""
+					Escolha uma opção no menu:
+					1 - Falar com o chat.
+					2 - Inserir Artistas no banco de dados.
+					3 - Insrir Musicas no banco de dados.
+					0 - sair
+					""");
+			int option = scanner.nextInt();
+			scanner.nextLine();
+			switch (option) {
+				case 1:
+					talkToChat();
+					break;
+				case 2:
+					break;
+				case 0:
+					scanner.close();
+					System.exit(0);
+
+				default:
+					System.out.println("Opção inválida.");
+					break;
+
+			}
+		}
+	}
+	public void talkToChat(){
+		String ask = "";
+		while(true){
+			System.out.println("*** Faça uma pergunta ao chat? ***");
+			ask = scanner.nextLine();
+			if(!ask.equals("0")){
+				String response = gpt.askChat(ask);
+				System.out.println(response);
+			}
+			else {break;}
+		}
 	}
 }
